41e0756ed8af288cfc529f74bd2b80d06fbfa320
ythiero <35019@ma-web.nl>
2022-10-28 13:30:53 +0200

    niet klaar

---
+++ textgame.py
@@ -0,0 +1,183 @@
+print("BEEP! BEEP! BEEP! BEEP!")
+print("Je slaat  je wekker zodat het stopt en staat op poets. Je tanden zo als alltijd trekt kleding aan.  en neemt de dus die je altijd neemt.")
+print("Je  komt thuis  na dat je een 6 uur niks leep te doen. in je kantoor en ziet een stapel aan rot brieven die je niks boeit")
+print("op de bodem van de stappel zie je een goude ")
+print("het is een uitnodging naar een baan je leest niet verder en tekent gelijk de brief en vertrekt een week later ")
+print("je bent verast met het feit dat je in een raket zit die naar de maan gaat :had ik maar die brief gelezen: ")
+print ("de volginde word je een flesje pillen en een fles millo? water ga je de pillen in nemen ")
+
+print("A:gimme them pills!|B:nah i'm a rebel|c:neem er maar één ")
+
+A="A"
+B="B"
+C="C"
+X = input()
+if X ==B:
+    input("will je well de millo water A yes|B no ")
+    if X ==A:
+        input("je drinkt het water en je voelt je goed ")
+    if X ==B:
+        print("je voelt je niet goed en :pop:")
+        print("MMMHHH DIE PILLEN LIJKE ME BELANERIJK")
+        exit()
+if X ==A:
+    print("je neemt alle pillen en word misselijk")
+if X==C:
+    print("je neemt één pill")
+
+print("bing-bong-bing ")
+print(";alle medewerkers worden verwacht bij de test kamer;")
+print("je luister naan de intercom en gaat naar de testkamer ")
+print(":een man in een zwart pak:hallo ik hope dat iedereen een goede nacht had en klaar bent voor de uit leg van vandaag")
+print("ga je slapen door de uitleg y=A|n=B")
+if X ==A:
+    print("je hebt niet geluister en weet nu niet ")
+if X =="B":
+    print("vandaag gaat iedereen een vragen lijst invullen en in de farm tuin werken ")
+    print("de man praat nog een half uur verder en ook all vecht je er tegen je valt in slaap heel vreemd?")    
+
+print("je word waker en krijgt een boek je voor je met twee bladjes er in. en doet wat iedereen ")
+print("je beantwoord de vragen want je mag niet weg tot je ze beantwoord hebt")
+print("1# hoe voel je je ")
+if X =="A":
+    print("goed")
+if X =="B":
+    print("als of ik dood ga" )
+    print("wat zijn deze antworden!")
+
+print("BEEP! BEEP! BEEP! BEEP!")
+print("Je slaat  je wekker zodat het stopt en staat op poets. Je tanden zo als alltijd trekt kleding aan.  en neemt de dus die je altijd neemt.")
+print("Je  komt thuis  na dat je een 6 uur niks leep te doen. in je kantoor en ziet een stapel aan rot brieven die je niks boeit")
+print("op de bodem van de stappel zie je een goude ")
+print("het is een uitnodging naar een baan je leest niet verder en tekent gelijk de brief en vertrekt een week later ")
+print("je bent verast met het feit dat je in een raket zit die naar de maan gaat :had ik maar die brief gelezen: ")
+print ("de volginde word je een flesje pillen en een fles millo? water ga je de pillen in nemen ")
+
+print("A:gimme them pills!|B:nah i'm a rebel|c:neem er maar één ")
+
+A="A"
+B="B"
+C="C"
+X = input()
+if X ==B:
+    input("will je well de millo water A yes|B no ")
+    if X ==A:
+        input("je drinkt het water en je voelt je goed ")
+    if X ==B:
+        print("je voelt je niet goed en :pop:")
+        print("MMMHHH DIE PILLEN LIJKE ME BELANERIJK")
+        exit()
+if X ==A:
+    print("je neemt alle pillen en word misselijk")
+if X==C:
+    print("je neemt één pill")
+
+print("bing-bong-bing ")
+print(";alle medewerkers worden verwacht bij de test kamer;")
+print("je luister naan de intercom en gaat naar de testkamer ")
+print(":een man in een zwart pak:hallo ik hope dat iedereen een goede nacht had en klaar bent voor de uit leg van vandaag")
+print("ga je slapen door de uitleg y=A|n=B")
+if X ==A:
+    print("je hebt niet geluister en weet nu niet ")
+if X =="B":
+    print("vandaag gaat iedereen een vragen lijst invullen en in de farm tuin werken ")
+    print("de man praat nog een half uur verder en ook all vecht je er tegen je valt in slaap heel vreemd?")    
+
+print("je word waker en krijgt een boek je voor je met twee bladjes er in. en doet wat iedereen ")
+print("je beantwoord de vragen want je mag niet weg tot je ze beantwoord hebt")
+print("1# hoe voel je je ")
+if X =="A":
+    print("goed")
+if X =="B":
+    print("als of ik dood ga" )
+    print("wat zijn deze antworden!")print("BEEP! BEEP! BEEP! BEEP!")
+print("Je slaat  je wekker zodat het stopt en staat op poets. Je tanden zo als alltijd trekt kleding aan.  en neemt de dus die je altijd neemt.")
+print("Je  komt thuis  na dat je een 6 uur niks leep te doen. in je kantoor en ziet een stapel aan rot brieven die je niks boeit")
+print("op de bodem van de stappel zie je een goude ")
+print("het is een uitnodging naar een baan je leest niet verder en tekent gelijk de brief en vertrekt een week later ")
+print("je bent verast met het feit dat je in een raket zit die naar de maan gaat :had ik maar die brief gelezen: ")
+print ("de volginde word je een flesje pillen en een fles millo? water ga je de pillen in nemen ")
+
+print("A:gimme them pills!|B:nah i'm a rebel|c:neem er maar één ")
+
+A="A"
+B="B"
+C="C"
+X = input()
+if X ==B:
+    input("will je well de millo water A yes|B no ")
+    if X ==A:
+        input("je drinkt het water en je voelt je goed ")
+    if X ==B:
+        print("je voelt je niet goed en :pop:")
+        print("MMMHHH DIE PILLEN LIJKE ME BELANERIJK")
+        exit()
+if X ==A:
+    print("je neemt alle pillen en word misselijk")
+if X==C:
+    print("je neemt één pill")
+
+print("bing-bong-bing ")
+print(";alle medewerkers worden verwacht bij de test kamer;")
+print("je luister naan de intercom en gaat naar de testkamer ")
+print(":een man in een zwart pak:hallo ik hope dat iedereen een goede nacht had en klaar bent voor de uit leg van vandaag")
+print("ga je slapen door de uitleg y=A|n=B")
+if X ==A:
+    print("je hebt niet geluister en weet nu niet ")
+if X =="B":
+    print("vandaag gaat iedereen een vragen lijst invullen en in de farm tuin werken ")
+    print("de man praat nog een half uur verder en ook all vecht je er tegen je valt in slaap heel vreemd?")    
+
+print("je word waker en krijgt een boek je voor je met twee bladjes er in. en doet wat iedereen ")
+print("je beantwoord de vragen want je mag niet weg tot je ze beantwoord hebt")
+print("1# hoe voel je je ")
+if X =="A":
+    print("goed")
+if X =="B":
+    print("als of ik dood ga" )
+    print("wat zijn deze antworden!")print("BEEP! BEEP! BEEP! BEEP!")
+print("Je slaat  je wekker zodat het stopt en staat op poets. Je tanden zo als alltijd trekt kleding aan.  en neemt de dus die je altijd neemt.")
+print("Je  komt thuis  na dat je een 6 uur niks leep te doen. in je kantoor en ziet een stapel aan rot brieven die je niks boeit")
+print("op de bodem van de stappel zie je een goude ")
+print("het is een uitnodging naar een baan je leest niet verder en tekent gelijk de brief en vertrekt een week later ")
+print("je bent verast met het feit dat je in een raket zit die naar de maan gaat :had ik maar die brief gelezen: ")
+print ("de volginde word je een flesje pillen en een fles millo? water ga je de pillen in nemen ")
+
+print("A:gimme them pills!|B:nah i'm a rebel|c:neem er maar één ")
+
+A="A"
+B="B"
+C="C"
+X = input()
+if X ==B:
+    input("will je well de millo water A yes|B no ")
+    if X ==A:
+        input("je drinkt het water en je voelt je goed ")
+    if X ==B:
+        print("je voelt je niet goed en :pop:")
+        print("MMMHHH DIE PILLEN LIJKE ME BELANERIJK")
+        exit()
+if X ==A:
+    print("je neemt alle pillen en word misselijk")
+if X==C:
+    print("je neemt één pill")
+
+print("bing-bong-bing ")
+print(";alle medewerkers worden verwacht bij de test kamer;")
+print("je luister naan de intercom en gaat naar de testkamer ")
+print(":een man in een zwart pak:hallo ik hope dat iedereen een goede nacht had en klaar bent voor de uit leg van vandaag")
+print("ga je slapen door de uitleg y=A|n=B")
+if X ==A:
+    print("je hebt niet geluister en weet nu niet ")
+if X =="B":
+    print("vandaag gaat iedereen een vragen lijst invullen en in de farm tuin werken ")
+    print("de man praat nog een half uur verder en ook all vecht je er tegen je valt in slaap heel vreemd?")    
+
+print("je word waker en krijgt een boek je voor je met twee bladjes er in. en doet wat iedereen ")
+print("je beantwoord de vragen want je mag niet weg tot je ze beantwoord hebt")
+print("1# hoe voel je je ")
+if X =="A":
+    print("goed")
+if X =="B":
+    print("als of ik dood ga" )
+    print("wat zijn deze antworden!")
